80b62ff23a8cae4f26ba631e46d76cce944ce07e
ITH2QY Ratkai Kristof <ITH2QY@neptun.gdf.hu>
2023-05-20 12:24:55 +0200

    életerő körönkénti mutatása győztes kiíratása

--- src/me/RatkaiKristof/Jatek.java
+++ src/me/RatkaiKristof/Jatek.java
@@ -5,14 +5,21 @@ public class Jatek {
         Harcos harcos = new Harcos();
         Varazslo varazslo = new Varazslo();
 
-        System.out.println("H_V --> H: " + harcos.getEletero() + ", V: " + varazslo.getEletero());
-
         while (harcos.getEletero() > 0 && varazslo.getEletero() > 0) {
+            System.out.println("H_V --> H: " + harcos.getEletero() + ", V: " + varazslo.getEletero());
             harcos.tamad(varazslo);
             varazslo.tamad(harcos);
             System.out.println("X__ Harc: H:" + harcos.getEletero() + ", V:" + varazslo.getEletero());
-
         }
 
+        if (harcos.getEletero() <= 0 && varazslo.getEletero() <= 0) {
+            System.out.println("Döntetlen!");
+        }
+        else if (harcos.getEletero() <= 0){
+            System.out.println("A Varazsló gyözött!");
+        }
+        else if (varazslo.getEletero() <=0){
+            System.out.println("A Harcos gyözött!");
+        }
     }
 }
